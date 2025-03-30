73cc9c03fd2ab5dc391451b413f189da5c311d80
Soycloud1 <60202275+nicocasa27@users.noreply.github.com>
2025-03-30 11:27:32 -0600

    Update StoreSelector.tsx

--- src/components/users/StoreSelector.tsx
+++ src/components/users/StoreSelector.tsx
@@ -1,17 +1,5 @@
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Control } from "react-hook-form";
 import { RoleAssignmentValues } from "./validation/roleSchemas";
 
