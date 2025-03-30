d37834ebba6d6ca569bfe8d9a82397f9041d275b
nicocasa27 <60202275+nicocasa27@users.noreply.github.com>
2025-03-30 12:07:30 -0600

    tengo miedo

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { TooltipProvider } from '@radix-ui/react-tooltip'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
+  </React.StrictMode>,
+)
